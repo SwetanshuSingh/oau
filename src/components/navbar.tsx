49ddfc717637d6e8d31578d7b597f6faf015c1d7
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-11-10 15:57:25 +0530

    update: minor changes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   const items = ["oau", "home", "initiatives", "work"];
 
   return (
-    <nav className="w-full flex justify-start items-center text-white gap-12">
+    <nav className="w-full flex justify-start items-center text-white/80 gap-12">
       <p className="text-4xl font-bold uppercase">OAU</p>
       <span className="text-3xl font-semibold text-white/80">/</span>
       <p className="text-4xl font-bold uppercase cursor-pointer">Home</p>
