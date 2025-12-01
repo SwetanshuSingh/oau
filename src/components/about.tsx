5f2fd9c48cf0c538911c9e01bdc0d9eda0f928a5
SwetanshuSingh <swetanshusingh17@gmail.com>
2025-12-01 08:36:34 +0530

    update: minor changes

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,21 +1,23 @@
 export default function About() {
   return (
-    <section className="w-full bg-white p-10 flex flex-col gap-10">
-      <button className="w-fit py-2 px-5 bg-[#1E1919] text-white rounded-3xl text-xl uppercase">
-        <p>About Us</p>
-      </button>
-
-      <div className="w-full flex gap-8">
+    <section className="w-full p-10">
+      <div className="w-full flex gap-10">
         {/* Left Section */}
-        <div className="w-4/6 flex flex-col gap-6">
-          <h3 className="text-neutral-800 font-medium text-2xl italic text-justify">
+        <div className="w-3/6 flex flex-col gap-6">
+          <h3 className="text-neutral-800 font-medium text-3xl text-justify">
             Office for Objects Architecture Urbanism (OAU) is an international
             architectural, urban planning/design practice established to
             initiate works that mediate between various scales from objects,
             architecture, & urban planning & design.
           </h3>
 
-          <div className="flex flex-col gap-2">
+          <button className="w-fit py-2 px-5 text-[#1E1919] border border-[#1E1919] rounded-3xl text-xl hover:bg-[#1E1919] hover:text-white transition-colors duration-150">
+            <p>Our Services</p>
+          </button>
+        </div>
+
+        <div className="w-3/6 flex">
+          <div className="flex flex-col gap-4">
             <p className="text-neutral-600 text-lg text-justify tracking-tight">
               Throughout history, architecture & build mass has always been at
               the forefront of guiding civilizations, enriching experiences with
@@ -33,20 +35,23 @@ export default function About() {
               spaces that answer questions of our known & unknown perceptive
               environment through context research and design strategy.
             </p>
-          </div>
 
-          <button className="w-fit py-2 px-5 text-[#1E1919] border border-[#1E1919] rounded-3xl text-xl hover:bg-[#1E1919] hover:text-white transition-colors duration-150">
-            <p>Our Services</p>
-          </button>
-        </div>
-
-        <div className="w-2/6 flex">
-          <div className="relative w-full h-full">
-            <img
-              src="https://images.unsplash.com/photo-1763286056614-0fc228bf7139?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="OAU showcase"
-              className="w-full h-full object-cover"
-            />
+            <p className="text-neutral-600 text-lg text-justify tracking-tight">
+              Our architecture is an art of optimism, demanding of us a
+              forward-looking attitude and the nerve to stand out of sync with
+              present norms. We believe the significant work of reconciling past
+              traumas and addressing contemporary discord requires an
+              architecture that posits a more ideal world. Doing so is a
+              naturally enriching process for us and our partners, as we
+              collectively seek to expand our definitions of possibility toward
+              the betterment of our clients, their institutions, and the
+              cultures of which we are all part. As we live in an increasingly
+              divided society, there is a tendency to address this division by
+              counterproductively retreating into provincial understandings of
+              identity. In contrast to current provincialisms, the vocation of
+              modern architecture was, is, and should be cosmopolitan, committed
+              to enhancing the lives of all people everywhere.
+            </p>
           </div>
         </div>
       </div>
